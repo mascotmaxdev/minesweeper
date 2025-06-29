ef02e557e7b52636a4c91cd856a22d8f50650f1a
Webster Lusotan <wlusotan@gmail.com>
2025-06-30 03:45:18 +0800

    Added functions for click event

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const grid = [];
 const grid2 = [];
 
-let game = "unfinished";
+let game = "playing";
 class Cells {
   constructor(isBomb, counter, revealed) {
     this.isBomb = isBomb;
@@ -41,7 +41,7 @@ function createTilesNumbers() {
     for (let j = 0; j < 9; j++) {
       // Copy counter value
       if (grid[i][j].isBomb) {
-        rowx.push((grid[i][j].counter = "B"));
+        rowx.push((grid[i][j].counter = "💣"));
       } else {
         rowx.push(grid[i][j].counter);
       }
@@ -51,21 +51,6 @@ function createTilesNumbers() {
 
   console.table(grid2);
 }
-
-// function createTilesNumbers() {
-//   //-------------create separate table for bombs
-//   //Input number of equal rows and column
-//   for (let i = 0; i < 9; i++) {
-//     const rowx = [];
-//     for (let j = 0; j < 9; j++) {
-//       rowx.push(grid[i][j].counter);
-//       grid2[j] = grid[j].counter;
-//     }
-//     grid2.push(rowx);
-//   }
-
-//   console.table(grid2);
-// }
 
 function bombsVicinity() {
   for (let i = 0; i < 9; i++) {
@@ -89,68 +74,6 @@ function bombsVicinity() {
   }
   console.log(grid);
 }
-// function bombsVicinity() {
-//   // if (grid[0][0] === "wiw") {
-//   //   console.log("Not Wiw!");
-//   // }
-//   for (let i = 1; i < 9; i++) {
-//     for (let j = 1; j < 9; j++) {
-//       let countBombs = 0;
-//       if (!grid[i][j].isBomb) {
-//         if (
-//           grid[i - 1] &&
-//           grid[i - 1][j - 1] &&
-//           grid[i - 1][j - 1].isBomb === true
-//         ) {
-//           countBombs++;
-//           // grid[i][j].counter++;
-//           // console.log(grid[i][j].counter);
-//           //    console.log(countBombs++);
-//         }
-//         if (grid[i - 1] && grid[i - 1][j] && grid[i - 1][j].isBomb === true) {
-//           countBombs++;
-//           //  console.log(countBombs++);
-//         }
-//         if (
-//           grid[i - 1] &&
-//           grid[i - 1][j + 1] &&
-//           grid[i - 1][j + 1].isBomb === true
-//         ) {
-//           countBombs++;
-//         }
-//         if (grid[i] && grid[i][j - 1] && grid[i][j - 1].isBomb === true) {
-//           countBombs++;
-//         }
-//         if (grid[i] && grid[i][j + 1] && grid[i][j + 1].isBomb === true) {
-//           countBombs++;
-//         }
-//         if (
-//           grid[i + 1] &&
-//           grid[i + 1][j - 1] &&
-//           grid[i + 1][j - 1].isBomb === true
-//         ) {
-//           countBombs++;
-//         }
-//         if (grid[i + 1] && grid[i + 1][j] && grid[i + 1][j].isBomb === true) {
-//           grid[i][j].counter++;
-//           countBombs++;
-//         }
-
-//         if (
-//           grid[i + 1] &&
-//           grid[i + 1][j + 1] &&
-//           grid[i + 1][j + 1].isBomb === true
-//         ) {
-//           countBombs++;
-//         }
-//         if (countBombs > 0) {
-//           grid[i][j].counter = countBombs;
-//         }
-//       }
-//     }
-//   }
-//   console.table(grid);
-// }
 
 function gameState() {
   for (let i = 0; i < 9; i++) {
@@ -171,76 +94,79 @@ function clickedCoordinates(x, y) {
 
   if (!cell.isBomb) {
     cell.revealed = true;
+    //  console.log(`revealed`);
   } else if (cell.isBomb) {
     console.log("Game is over you moron!!");
+    cell.revealed = true;
+    game = "finished";
+    console.log(game);
+    return;
   } else if (!cell.isBomb && game === "finished") {
     cell.revealed = true;
     console.log("Congratulations you win!");
   }
 }
-// function clickedCoordinates(x, y) {
-//   if (!grid[x] || !grid[x][y]) return; // avoid errors on out-of-bounds
 
-//   if (grid[x] && grid[x][y] && !grid[x][y].isBomb) {
-//     grid[x][y].revealed = true;
-//   } else if (grid[x] && grid[x][y].isBomb && game === "finished") {
-//     grid[x][y].revealed = true;
-//     console.log("Congratulations you win!");
-//   } else if (grid[x] && grid[x][y].isBomb === true) {
-//     console.log("Game is over you moron!!");
-//   }
-// }
+const container = document.querySelector(".container");
 
-// function clickedCoordinates(x, y) {
-//   if (grid[x][y].isBomb === false) {
-//     grid[x][y].revealed = true; //changes X to O means its opened
-//   }
-//   if (grid[x][y].isBomb === true && game === "finished") {
-//     grid[x][y].revealed = true; //changes X to O means its opened
-//     console.log("Congratulations you win!");
-//   }
-//   if (grid[x][y].isBomb === true) {
-//     console.log(`Game is over you moron!!`);
-//   }
-// }
+function renderBoard() {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      let button = document.createElement("button");
+      button.id = `${i},${j}`;
+      button.textContent = `${grid[i][j].counter}`;
+      button.classList.add(`revealed-${grid[i][j].revealed}`);
+      // if (grid[i][j].revealed === true) {
+      //   button.classList.add("revealed-true");
+      // }
+      // button.classList.add(`isbomb-${grid[i][j].isBomb}`);
+      // button.classList.add(`revealed-${grid[i][j].revealed}`);
+      // button.style.color = "transparent";
+      // button.dataset.col = `${i}`;
+      // button.dataset.row = `${j}`;
+      // button.dataset.counter = `${grid[i][j].counter}`;
+
+      // if (grid[i][j].counter === 0) {
+      //   button.style.color = "transparent";
+      // }
+      // if (grid[i][j].counter === 1) {
+      //   button.style.color = "blue";
+      // }
+      // if (grid[i][j].counter === 2) {
+      //   button.style.color = "green";
+      // }
+      // if (grid[i][j].counter === 3) {
+      //   button.style.color = "red";
+      // }
+      // if (grid[i][j].counter === 4) {
+      //   button.style.color = "purple";
+      // }
+      container.append(button);
+      console.log(`Konoha`);
+    }
+  }
+}
+
+container.addEventListener("click", (button) => {
+  const target = button.target;
+  const [rowStr, colStr] = target.id.split(",");
+  //console.log(grid[rowStr][colStr].revealed);
+  clickedCoordinates(rowStr, colStr);
+  target.classList.remove("revealed-false");
+  target.classList.add("revealed-true");
+  const allButtons = document.querySelectorAll("button");
+  console.log(grid[rowStr][colStr].revealed);
+  if (game === "finished") {
+    allButtons.forEach((btn) => {
+      btn.classList.remove("revealed-false");
+      btn.classList.add("revealed-true");
+    });
+  }
+});
 
 createTiles();
 bombsVicinity();
 gameState();
 createTilesNumbers();
+renderBoard();
 console.table(grid);
-clickedCoordinates(0, 8);
-clickedCoordinates(1, 5);
-clickedCoordinates(3, 7);
-clickedCoordinates(4, 8);
-clickedCoordinates(2, 6);
-clickedCoordinates(3, 7);
-clickedCoordinates(0, 8);
-clickedCoordinates(2, 5);
-clickedCoordinates(3, 4);
-clickedCoordinates(1, 8);
-clickedCoordinates(2, 5);
-clickedCoordinates(3, 7);
-clickedCoordinates(7, 8);
-clickedCoordinates(0, 5);
-clickedCoordinates(3, 1);
-clickedCoordinates(0, 1);
-clickedCoordinates(2, 5);
-clickedCoordinates(3, 7);
-clickedCoordinates(0, 8);
-clickedCoordinates(2, 5);
-clickedCoordinates(8, 2);
-clickedCoordinates(8, 1);
-clickedCoordinates(2, 5);
-clickedCoordinates(3, 1);
-
-// console.log(grid[0][2]);
-// console.log(grid[1][7]);
-// console.log(grid[2][2]);
-// console.log(grid[4][8]);
-// console.log(grid[7][4]);
-// console.log(grid[5][7]);
-// console.log(grid[3][3]);
-// console.log(grid[4][8]);
-// console.log(grid[1][2].counter);
-// console.table(grid);
