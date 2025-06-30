d80fff8d64cda5a0527cb457e7735e562f7a6b3c
Webster Lusotan <wlusotan@gmail.com>
2025-06-30 18:08:02 +0800

    Cleaned up coordinate xy function

--- script.js
+++ script.js
@@ -29,8 +29,6 @@ function createTiles() {
       bombsPlaced++;
     }
   }
-
-  // console.log(grid);
 }
 
 function createTilesNumbers() {
@@ -39,7 +37,6 @@ function createTilesNumbers() {
   for (let i = 0; i < 9; i++) {
     const rowx = [];
     for (let j = 0; j < 9; j++) {
-      // Copy counter value
       if (grid[i][j].isBomb) {
         rowx.push((grid[i][j].counter = "💣"));
       } else {
@@ -92,20 +89,43 @@ function clickedCoordinates(x, y) {
 
   const cell = grid[x][y];
 
-  if (!cell.isBomb) {
-    cell.revealed = true;
-    //  console.log(`revealed`);
-  } else if (cell.isBomb) {
-    console.log("Game is over you moron!!");
+  if (cell.isBomb) {
     cell.revealed = true;
     game = "finished";
-    console.log(game);
+    console.log("Game is over you moron!!");
     return;
-  } else if (!cell.isBomb && game === "finished") {
+  }
+
+  if (!cell.revealed) {
     cell.revealed = true;
+  }
+
+  if (gameState() === "finished") {
+    game = "won";
+    alert("Game won!");
     console.log("Congratulations you win!");
   }
 }
+
+// function clickedCoordinates(x, y) {
+//   if (!grid[x] || !grid[x][y]) return; // guard clause for out-of-bounds
+
+//   const cell = grid[x][y];
+//   //gameState();
+//   if (!cell.isBomb && gameState() !== "finished") {
+//     cell.revealed = true;
+//   } else if (!cell.isBomb && gameState() === "finished") {
+//     cell.revealed = true;
+//     game = "won";
+//     alert("Game won!");
+//     console.log("Congratulations you win!");
+//   } else if (cell.isBomb) {
+//     cell.revealed = true;
+//     game = "finished";
+//     console.log("Game is over you moron!!");
+//     return;
+//   }
+// }
 
 const container = document.querySelector(".container");
 
@@ -116,57 +136,79 @@ function renderBoard() {
       button.id = `${i},${j}`;
       button.textContent = `${grid[i][j].counter}`;
       button.classList.add(`revealed-${grid[i][j].revealed}`);
-      // if (grid[i][j].revealed === true) {
-      //   button.classList.add("revealed-true");
-      // }
-      // button.classList.add(`isbomb-${grid[i][j].isBomb}`);
-      // button.classList.add(`revealed-${grid[i][j].revealed}`);
-      // button.style.color = "transparent";
-      // button.dataset.col = `${i}`;
-      // button.dataset.row = `${j}`;
-      // button.dataset.counter = `${grid[i][j].counter}`;
-
-      // if (grid[i][j].counter === 0) {
-      //   button.style.color = "transparent";
-      // }
-      // if (grid[i][j].counter === 1) {
-      //   button.style.color = "blue";
-      // }
-      // if (grid[i][j].counter === 2) {
-      //   button.style.color = "green";
-      // }
-      // if (grid[i][j].counter === 3) {
-      //   button.style.color = "red";
-      // }
-      // if (grid[i][j].counter === 4) {
-      //   button.style.color = "purple";
-      // }
       container.append(button);
-      console.log(`Konoha`);
     }
   }
 }
 
+// const rightClick = document.querySelector("button"); -- RIGHT CLICK
+
+// container.addEventListener("contextmenu", (button) => {
+//   event.preventDefault(); // prevent the default right-click menu\
+//   const target = button.target;
+//   const [rowStr, colStr] = target.id.split(",");
+//   if (target.tagName !== "BUTTON") {
+//     return;
+//   }
+//   if (!target.classList.contains("tagged")) {
+//     target.classList.add("tagged");
+//   } else if (target.tagName === "BUTTON") {
+//     target.classList.remove("tagged");
+//   }
+//   //alert("Right-click detected!");
+// });
+
 container.addEventListener("click", (button) => {
   const target = button.target;
   const [rowStr, colStr] = target.id.split(",");
-  //console.log(grid[rowStr][colStr].revealed);
+
   clickedCoordinates(rowStr, colStr);
   target.classList.remove("revealed-false");
   target.classList.add("revealed-true");
   const allButtons = document.querySelectorAll("button");
   console.log(grid[rowStr][colStr].revealed);
+  if (grid[rowStr][colStr].counter === 0) {
+    target.style.color = "transparent";
+  }
+  if (grid[rowStr][colStr].counter === 1) {
+    target.style.color = "blue";
+  }
+  if (grid[rowStr][colStr].counter === 2) {
+    target.style.color = "green";
+  }
+  if (grid[rowStr][colStr].counter === 3) {
+    target.style.color = "red";
+  }
+  if (grid[rowStr][colStr].counter === 4) {
+    target.style.color = "purple";
+  }
   if (game === "finished") {
     allButtons.forEach((btn) => {
       btn.classList.remove("revealed-false");
       btn.classList.add("revealed-true");
+      btn.style.removeProperty("color");
+      btn.disabled = true;
     });
+    const loser = document.createElement("h3");
+    loser.innerHTML = `💥GAME OVER!💥`;
+    container.appendChild(loser);
+  }
+  if (gameState() === "finished" && game === "won") {
+    allButtons.forEach((btn) => {
+      btn.classList.remove("revealed-false");
+      btn.classList.add("revealed-true");
+      btn.style.removeProperty("color");
+      btn.disabled = true;
+    });
+    const loser = document.createElement("h3");
+    loser.innerHTML = `Congratulations you won!🏆🏆🏆`;
+    container.appendChild(loser);
   }
 });
 
 createTiles();
 bombsVicinity();
-gameState();
+//gameState();
 createTilesNumbers();
 renderBoard();
 console.table(grid);
